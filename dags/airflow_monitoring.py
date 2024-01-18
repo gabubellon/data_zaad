c239be1070c27605290435356466a0a30ac1c1b2
Gabu Bellon <gabubellon@gmail.com>
2024-01-18 20:28:37 -0300

    changes tests

--- dags/airflow_monitoring.py
+++ dags/airflow_monitoring.py
@@ -1,11 +1,12 @@
 """A liveness prober dag for monitoring composer.googleapis.com/environment/healthy."""
-import airflow
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
 from datetime import timedelta
 
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
 default_args = {
-    'start_date': airflow.utils.dates.days_ago(0),
+    'start_date': pendulum.today('UTC'),
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
 }
@@ -14,7 +15,7 @@ dag = DAG(
     'airflow_monitoring',
     default_args=default_args,
     description='liveness monitoring dag',
-    schedule_interval='*/10 * * * *',
+    schedule='*/10 * * * *',
     max_active_runs=2,
     catchup=False,
     dagrun_timeout=timedelta(minutes=10),
